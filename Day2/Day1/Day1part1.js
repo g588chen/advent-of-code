f19fed9b914228d674eae0764ebc6eb0ca056096
George Chen <g58chen@gmail.com>
2020-02-02 21:47:42 -0500

    Remove Duplicate

--- Day2/Day1/Day1part1.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var masses = new Array
-var fuel = 0
-fs.readFileSync('input.txt').toString().split('\n').forEach(function (line) { masses.push(line); })
-for (x of masses) {
-    fuel += Math.floor(x/3) - 2
-}
-console.log(fuel)
